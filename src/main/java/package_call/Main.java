522c712e560fb6f62ed348b257c268e48ec49f5c
Rosa Valenzuela <natsukiarr@gmail.com>
2023-03-29 18:46:50 -0700

    agrege los frames

---
+++ src/main/java/package_call/Main.java
@@ -0,0 +1,19 @@
+package package_call;
+
+import Frames.Principal;
+
+/**
+ *
+ * @author jctri
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal pl = new Principal(false);
+        pl.setVisible(true);
+    }
+    
+}
